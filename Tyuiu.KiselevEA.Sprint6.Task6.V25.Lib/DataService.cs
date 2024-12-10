137eb58cc595948fae94be14ce78b9ffefd268ec
Tyuiu.KiselevEA@outlook.com <Tyuiu.KiselevEA@outlook.com>
2024-12-10 17:45:06 +0500

    123

--- Tyuiu.KiselevEA.Sprint6.Task6.V25.Lib/DataService.cs
+++ Tyuiu.KiselevEA.Sprint6.Task6.V25.Lib/DataService.cs
@@ -4,8 +4,9 @@ namespace Tyuiu.KiselevEA.Sprint6.Task6.V25.Lib
 {
     public class DataService : ISprint6Task6V25
     {
-        public string CollectTextFromFile(string str, string path)
+        public string CollectTextFromFile(string path)
         {
+
             string resStr = "";
             using (StreamReader reader = new StreamReader(path))
             {
